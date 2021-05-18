6ad242b541d62e8939e6d9c6bcf8e0c63e53d8d2
Redtreex <sakibrahman86@gmail.com>
2021-05-18 11:58:04 +0600

    commit

--- TestCode/Admin.test.js
+++ TestCode/Admin.test.js
@@ -6,52 +6,52 @@ test('Testing Admin Class', () => {
     });
     checkBilling("ord231");
 
-    // emmitter.on("orderConfirm",(result) =>{
-    //     expect(result).toBe("confirmed");
-    // })
-    // orderConfirm("ord789");
+    emmitter.on("orderConfirm",(result) =>{
+        expect(result).toBe("confirmed");
+    })
+    orderConfirm("ord789");
 
-    // emmitter.on("orderCancel",(result) =>{
-    //     expect(result).toBe("cancelled");
-    // });
-    // orderCancel("ord789")
+    emmitter.on("orderCancel",(result) =>{
+        expect(result).toBe("cancelled");
+    });
+    orderCancel("ord789")
 
-    // emmitter.on("orderDelivered",(result) =>{
-    //     expect(result).toBe("delivered");
-    // });
-    // orderCancel("ord890")
+    emmitter.on("orderDelivered",(result) =>{
+        expect(result).toBe("delivered");
+    });
+    orderDelivery("ord890")
 
-    // emmitter.on("addEmployee", (result) => {
-    //     expect(result).toBe("employeeAdded");
-    // });
-    // addEmployee(
-    //     {
-    //         "FirstName": "Marufurur",
-    //         "LastName": "Rahman",
-    //         "Mail": "maruf69@gmail.com",
-    //         "Address": "Chittagong",
-    //         "NID": "NID",
-    //         "BirthDate": "12/7/1999",
-    //         "ContactNo": "017239232312",
-    //         "Password": "1234",
-    //         "Role": "Android Developer",
-    //         "MemberSince": "27/4/2021",
-    //         "E_ID": "E2104010",
-    //         "TeamLead": "Bodda Ahmed",
-    //         "level": 2,
-    //         "salary": 15000
-    //     }
-    // );
+    emmitter.on("addEmployee", (result) => {
+        expect(result).toBe("employeeAdded");
+    });
+    addEmployee(
+        {
+            "FirstName": "Marufurur",
+            "LastName": "Rahman",
+            "Mail": "maruf69@gmail.com",
+            "Address": "Chittagong",
+            "NID": "NID",
+            "BirthDate": "12/7/1999",
+            "ContactNo": "017239232312",
+            "Password": "1234",
+            "Role": "Android Developer",
+            "MemberSince": "27/4/2021",
+            "E_ID": "E2104010",
+            "TeamLead": "Bodda Ahmed",
+            "level": 2,
+            "salary": 15000
+        }
+    );
 
-    // emmitter.on("removeEmployee",(result) =>{
-    //     expect(result).toBe("removed");
-    // });
-    // remEmployee("E2104010")
+    emmitter.on("removeEmployee",(result) =>{
+        expect(result).toBe("removed");
+    });
+    remEmployee("E2104010")
 
-    // emmitter.on("editEmployee",(result) =>{
-    //     expect(result).toBe("updated");
-    // });
-    // remEmployee("E2104002",{ $set: {name: "Mahfuzur"}})
+    emmitter.on("editEmployee",(result) =>{
+        expect(result).toBe("updated");
+    });
+    editEmployee("E2104002",{ $set: {name: "Mahfuzur"}})
 
 })
 
--- TestCode/Employer.test.js
+++ TestCode/Employer.test.js
@@ -1,13 +1,13 @@
 const {emmitter, viewOrder, sendStatus } = require('../model/Employer');
 
 test('Employer class test', () => {
-    // emmitter.on("viewOrder",(result) =>{
-    //       expect(result).toBe("[Object object]")
-    // })
-    // viewOrder("ord231")
+    emmitter.on("viewOrder",(result) =>{
+          expect(result).toBe("[Object object]")
+    })
+    viewOrder("ord231")
 
-    // emmitter.on("sendStatus",(result) =>{
-    //       expect(result).toBe(1)
-    // })
-    // sendStatus("E2104003");
+    emmitter.on("sendStatus",(result) =>{
+          expect(result).toBe(1)
+    })
+    sendStatus("E2104003");
 })
